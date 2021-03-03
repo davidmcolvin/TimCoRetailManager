dca74d9e27b274d16c7b41747eb4bd4d92ce1074
David Colvin <davidmcolvin@gmail.com>
2021-03-02 21:53:27 -0500

    Add back Register main tables
    Fixed system generated naming and added all tables back

---
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+  [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SaleId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] MONEY NOT NULL, 
+    [PurchacePrice] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL
+)
