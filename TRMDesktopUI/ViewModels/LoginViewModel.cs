9951a658f3847d4705ce479b17dfeace8b51266f
David Colvin <davidmcolvin@gmail.com>
2021-05-30 22:00:49 -0400

    Add Log In Form
    Mocked up a basic login form to give us basic functionality

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Caliburn.Micro;
 
 namespace TRMDesktopUI.ViewModels
 {
-  public class ShellViewModel : Conductor<object>
+  public class LoginViewModel
   {
   }
 }
