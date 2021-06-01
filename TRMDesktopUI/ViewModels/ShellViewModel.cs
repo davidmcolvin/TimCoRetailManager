8d7abf70bc39ad11b0ef4a9670180443e759a63e
David Colvin <davidmcolvin@gmail.com>
2021-05-31 21:58:23 -0400

    Wire up log in form
    Added code to be able to call our LogIn functions.  Had to add some helpers because passwordbox was not working with caliburnMicro

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -9,5 +9,12 @@ namespace TRMDesktopUI.ViewModels
 {
   public class ShellViewModel : Conductor<object>
   {
+    private LoginViewModel _loginVM;
+
+    public ShellViewModel(LoginViewModel loginVM)
+    {
+      _loginVM = loginVM;
+      ActivateItem(_loginVM);
+    }
   }
 }
